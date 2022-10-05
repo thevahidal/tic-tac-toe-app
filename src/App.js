fca223398eede54a2f95f53f5a9454f678187991
Vahid Al <thevahidal@gmail.com>
2022-10-05 17:56:59 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
 import React from 'react'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import { useDispatch } from 'react-redux'
-
-import './App.css';
-import CustomRouter from './routes'
-import { UserContextProvider } from "./context/UserContext";
-import { GameContextProvider } from "./context/GameContext";
-
-const App = () => {
-
-  return (
-    <>
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
+import { ToastContainer } frocusLoss
         draggable
         pauseOnHover
       />
